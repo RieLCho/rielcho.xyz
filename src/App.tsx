2cb3f503aa1ad4d47145e3ea6fb6aefa08c6c973
Yangjin Cho <choyj1222@gmail.com>
2023-03-19 12:52:34 +0000

    feat: Created Resume.tsx & moved to pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Resume from "./pages/Resume";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Resume />
+    </>
   )
 }
