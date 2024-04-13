ae4ec2383607e1fb6e49a9cfbd78ba9a71003374
Yangjin Cho <choyj1222@gmail.com>
2024-04-13 03:42:41 +0000

    feat: prettier 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 const Footer = () => {
-  return (
-    <header className="bg-red-50 h-20">
-      <a href="https://sheepjin99.tistory.com/">Tistory</a>
-      <a href="https://sheepjin99.tistory.com/">Tistory</a>
-      <a href="https://sheepjin99.tistory.com/">Tistory</a>
-    </header>
-  );
-};
+    return (
+        <header className="bg-red-50 h-20">
+            <div>
+                <a href="https://sheepjin99.tistory.com/">Tistory</a>
+                <a href="https://github.com/">GitHub</a>
+                <a href="https://twitter.com/jini_QwQ">Twitter</a>
+            </div>
+        </header>
+    )
+}
 
 const Main = () => {
-  return <body className="bg-bg100">main</body>;
-};
+    return <body className="bg-bg100">main</body>
+}
 
 const App = () => {
-  return (
-    <div className="w-full h-full bg-bg100">
-      <Footer />
-      <Main />
-    </div>
-  );
-};
+    return (
+        <div className="w-full h-full bg-bg100">
+            <Footer />
+            <Main />
+        </div>
+    )
+}
 
-export default App;
+export default App
