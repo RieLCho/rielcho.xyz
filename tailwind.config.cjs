628ed78e33f3f7ed9de3b7a13880736466b4463e
Yangjin Cho <choyj1222@gmail.com>
2024-04-13 04:26:43 +0000

    feat: move color settings into extend scope.
    color settings was overriding default color theme.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        "maple-light": ["Maplestory Light"],
-        "maple-bold": ["Maplestory Bold"],
-      },
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'maple-light': ['Maplestory Light'],
+                'maple-bold': ['Maplestory Bold'],
+            },
+            colors: {
+                bg100: '#f7f8fd',
+                bg200: '#e8ebf6',
+                gd: 'linear-gradient(270deg, #00DC7A 0%, #00C3A1 33.33%, #119FFF 66.67%, #0076FF 100%);',
+                gdh: 'linear-gradient(270deg, #04D363 0%, #04AAA4 33.33%, #0B8AF8 66.67%, #0163FF 100%);',
+            },
+        },
     },
-    colors: {
-      bg100: "#f7f8fd",
-      bg200: "#e8ebf6",
-      gd: "linear-gradient(270deg, #00DC7A 0%, #00C3A1 33.33%, #119FFF 66.67%, #0076FF 100%);",
-      gdh: "linear-gradient(270deg, #04D363 0%, #04AAA4 33.33%, #0B8AF8 66.67%, #0163FF 100%);",
-    },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
