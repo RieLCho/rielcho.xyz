6b7a8b418bf356150ba3fc25a0ee4770f84929b7
Yangjin Cho <choyj1222@gmail.com>
2024-04-13 04:04:48 +0000

    feat: Main 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-const Footer = () => {
+const Header = () => {
     return (
-        <header className="bg-red-50 h-20">
-            <div>
+        <header className="h-20 sticky top-10">
+            <div className="flex gap-4 justify-end">
                 <a href="https://sheepjin99.tistory.com/">Tistory</a>
                 <a href="https://github.com/">GitHub</a>
                 <a href="https://twitter.com/jini_QwQ">Twitter</a>
@@ -11,14 +11,24 @@ const Footer = () => {
 }
 
 const Main = () => {
-    return <body className="bg-bg100">main</body>
+    return (
+        <body className="h-screen w-full">
+            <div>Yangjin Cho</div>
+            <div>좋아하는 것에 진심인, 꿈나무 개발자 조양진입니다.</div>
+        </body>
+    )
+}
+
+const Footer = () => {
+    return <footer className="">footer</footer>
 }
 
 const App = () => {
     return (
-        <div className="w-full h-full bg-bg100">
-            <Footer />
+        <div className="w-full h-full p-10 bg-bg100">
+            <Header />
             <Main />
+            <Footer />
         </div>
     )
 }
