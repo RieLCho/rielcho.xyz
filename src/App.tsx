5d583e9a5d7ea01b8585dad86ff549c99a332196
Yangjin Cho <choyj1222@gmail.com>
2024-04-13 04:27:03 +0000

    feat: move PersonalInfo.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { PersonalInfo } from './main/PersonalInfo'
+
 const Header = () => {
     return (
         <header className="h-20 sticky top-10">
-            <div className="flex gap-4 justify-end">
-                <a href="https://sheepjin99.tistory.com/">Tistory</a>
-                <a href="https://github.com/">GitHub</a>
-                <a href="https://twitter.com/jini_QwQ">Twitter</a>
+            <div className="flex gap-4 justify-end font-maple-light">
+                <a target="_blank" href="https://sheepjin99.tistory.com/">
+                    Tistory
+                </a>
+                <a target="_blank" href="https://github.com/RieLCho">
+                    GitHub
+                </a>
+                <a target="_blank" href="https://twitter.com/jini_QwQ">
+                    Twitter
+                </a>
             </div>
         </header>
     )
@@ -13,23 +21,26 @@ const Header = () => {
 const Main = () => {
     return (
         <body className="h-screen w-full">
-            <div>Yangjin Cho</div>
-            <div>좋아하는 것에 진심인, 꿈나무 개발자 조양진입니다.</div>
+            <div className="p-10">
+                <PersonalInfo />
+            </div>
         </body>
     )
 }
 
 const Footer = () => {
-    return <footer className="">footer</footer>
+    return <footer className="bg-black">footer</footer>
 }
 
 const App = () => {
     return (
-        <div className="w-full h-full p-10 bg-bg100">
-            <Header />
-            <Main />
+        <>
+            <div className="w-full h-full p-10 bg-bg100">
+                <Header />
+                <Main />
+            </div>
             <Footer />
-        </div>
+        </>
     )
 }
 
---
+++ src/main/PersonalInfo.tsx
@@ -0,0 +1,13 @@
+export const PersonalInfo = () => {
+    return (
+        <div className="space-y-4 flex-col pt-10">
+            <img
+                className="rounded-full w-56 h-56"
+                src="https://avatars.githubusercontent.com/u/13748138?s=400&u=cfa7aa7b0688ef674d8eb487bbe27af132f39093&v=4"
+                alt="profile"
+            />
+            <div className="font-maple-bold text-7xl">Yangjin Cho</div>
+            <div className="font-maple-light text-2xl">좋아하는 것에 진심인, 꿈나무 개발자 조양진입니다.</div>
+        </div>
+    )
+}
