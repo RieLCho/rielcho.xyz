81a075904f556838e123884c3cb846f90e8ab62b
Yangjin Cho <choyj1222@gmail.com>
2024-06-30 14:17:09 +0900

    Feature/add link (#10)
    * feat: added some links
    
    * feat: added links
    
    * fix: education status

--- src/main/WorkExperiences.tsx
+++ src/main/WorkExperiences.tsx
@@ -12,6 +12,38 @@ export const WorkExperiences = () => {
             <div className="pl-5">
                 <Italic>2021.08 ~ (재직 중)</Italic>
             </div>
+            <div className="pl-5">
+                <SubTitle>
+                    <a href={'https://www.gamescale.io/ko/products/11'} target="_blank">
+                        <SubTitle>
+                            <Dot>
+                                <span className="underline">AD Creator 프론트엔드 개발</span>
+                            </Dot>
+                        </SubTitle>
+                    </a>
+                    <a href={'https://creators.nexon.com'} target="_blank">
+                        <SubTitle>
+                            <Dot>
+                                <span className="underline">넥슨 크리에이터즈 프론트엔드 개발</span>
+                            </Dot>
+                        </SubTitle>
+                    </a>
+                    <div className="pl-5">
+                        <a
+                            href={'https://www.intelligencelabs.tech/86c22758-0540-4732-be7c-2494a44b893e'}
+                            target="_blank"
+                        >
+                            <SubTitle>
+                                <Dot>
+                                    <span className="underline">
+                                        넥슨 크리에이터즈 플랫폼의 전세계 도약을 위한 검색 엔진 최적화 여정
+                                    </span>
+                                </Dot>
+                            </SubTitle>
+                        </a>
+                    </div>
+                </SubTitle>
+            </div>
         </div>
     );
 };
