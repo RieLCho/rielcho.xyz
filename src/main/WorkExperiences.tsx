1818731041d75ee53b229a4ae19b8f76deb1f050
Yangjin Cho <choyj1222@gmail.com>
2024-07-09 00:48:53 +0900

    feat: added tfd link (#13)

--- src/main/WorkExperiences.tsx
+++ src/main/WorkExperiences.tsx
@@ -7,7 +7,12 @@ export const WorkExperiences = () => {
         <div className="flex flex-col gap-2">
             <Title>Work Experiences</Title>
             <SubTitle>
-                <Dot>NGINE STUDIOS @ NEXON COMPANY</Dot>
+                <Dot>
+                    NGINE STUDIOS @ NEXON COMPANY{' '}
+                    <a href={'https://career.nexon.com/user/recruit/member/postList?joinCorp=GN'} target="_blank">
+                        <span className="underline">(채용 공고)</span>
+                    </a>
+                </Dot>
             </SubTitle>
             <div className="pl-5">
                 <Italic>2021.08 ~ (재직 중)</Italic>
@@ -38,6 +43,15 @@ export const WorkExperiences = () => {
                                     <span className="underline">
                                         넥슨 크리에이터즈 플랫폼의 전세계 도약을 위한 검색 엔진 최적화 여정
                                     </span>
+                                </Dot>
+                            </SubTitle>
+                        </a>
+                    </div>
+                    <div className="pl-5">
+                        <a href={'https://x.com/jini_QwQ/status/1808043158485586000'} target="_blank">
+                            <SubTitle>
+                                <Dot>
+                                    <span className="underline">게임 The First Descendant 개발자 크레딧 등재</span>
                                 </Dot>
                             </SubTitle>
                         </a>
