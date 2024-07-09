d18b905b2b4ebd17d69bb4dffd71f75e7ce3f7e7
Yangjin Cho <choyj1222@gmail.com>
2024-07-09 04:23:01 +0000

    feat: css 수정

--- src/main/PersonalInfo.tsx
+++ src/main/PersonalInfo.tsx
@@ -1,6 +1,6 @@
 export const PersonalInfo = () => {
     return (
-        <div className="2xl:space-y-4 xl:space-y-4 lg:space-y-4 md:space-y-2 sm:space-y-2 space-y-2 flex-col pt-10">
+        <div className="2xl:space-y-4 xl:space-y-4 lg:space-y-4 md:space-y-2 sm:space-y-2 space-y-0 flex-col pt-10 sm:pt-4">
             <img
                 className="rounded-full 2xl:w-56 2xl:h-56 xl:w-56 xl:h-56 lg:w-56 lg:h-56 md:w-56 md:h-56 sm:w-32 sm:h-32 w-32 h-32"
                 src="https://avatars.githubusercontent.com/u/13748138?s=400&u=cfa7aa7b0688ef674d8eb487bbe27af132f39093&v=4"
